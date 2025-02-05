bb5d3640422c58d168c24f4c5b2cfa692cb9e34c
aleks1303 <aleksbulda13@gmail.com>
2025-02-05 16:34:54 +0200

    lesson1 hw1

--- lesson1/hw-1/main.js
+++ lesson1/hw-1/main.js
@@ -1,1 +1,49 @@
-console.log('dkd')
+// #dYQNrBV
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+//
+// #6Qb97gsv
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+//
+// #4N0y5tufA
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+//
+// #ruUtWDUI
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими І`мя, По-Батькові та роками. та вивести в консоль
+
+let a = 'hello';
+let b = 'own';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = true;
+let m = false;
+
+console.log(a, b, c, d, e, f, g, h, i, j, k, l, m);
+
+
+
+
+const firstName = "Olexander";
+const middleName = 'Sergiiovych';
+const lastName = 'Bulda';
+const person = `${firstName} ${middleName} ${lastName}`
+console.log(person)
+
+
+let x = 100;
+let y = '100';
+let z = true
+
+console.log(typeof (x))
+console.log(typeof (y))
+console.log(typeof (z))
+
