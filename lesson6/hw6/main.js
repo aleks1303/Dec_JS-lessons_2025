c0d6f72f1a91564dc3edb332bedb71d6121ed86d
aleks1303 <aleksbulda13@gmail.com>
2025-02-14 16:56:46 +0200

    lesson5

--- lesson6/hw6/main.js
+++ lesson6/hw6/main.js
@@ -1,17 +1,48 @@
+'use strict'
 // #dFeorS3m7u
 // - Знайти та вивести довжину наступних стрінгових значень
 // 'hello world', 'lorem ipsum', 'javascript is cool'
+let str1 = 'hello world';
+console.log(str1.length); // 11
+
+let str2 = 'lorem ipsum';
+console.log(str2.length) // 11
+
+let str3 = 'javascript is cool';
+console.log(str3.length) // 18
+
+
 // #8lld9HMxXWB
 // - Перевести до великого регістру наступні стрінгові значення
 // 'hello world', 'lorem ipsum', 'javascript is cool'
+let str4 = 'hello world';
+console.log(str4.toUpperCase());
+
+let str5 = 'lorem ipsum';
+console.log(str5.toUpperCase());
+
+let str6 = 'javascript is cool';
+console.log(str6.toUpperCase());
+
+
 // #ClDsAm7xba7
 // - Перевести до нижнього регістру настипні стрінгові значення
 // 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+let str7 = 'HELLO WORLD';
+console.log(str4.toLowerCase());
+
+let str8 = 'LOREM IPSUM';
+console.log(str5.toLowerCase());
+
+let str9 = 'JAVASCRIPT IS COOL';
+console.log(str6.toLowerCase());
+
+
 // #0b89BkYZwu
 // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
 //
 //     #bfoJuse4ZzP
-// - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+// - Напишіть функцію stringToArray(str), яка перетворює рядок на масив слів.
 //     let str = 'Ревуть воли як ясла повні';
 // let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
 //
