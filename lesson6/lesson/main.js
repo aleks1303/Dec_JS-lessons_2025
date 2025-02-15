c5651fbd8207cce0220ee5c7b3032bfa05da107d
aleks1303 <aleksbulda13@gmail.com>
2025-02-15 16:28:57 +0200

    lesson 6

--- lesson6/lesson/main.js
+++ lesson6/lesson/main.js
@@ -320,41 +320,41 @@
 //
 // функція фільтр з callback
 //
-let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
-];
-
-
-function filter (arr, callback) {
-    let mass = []
-    for (const item of arr) {
-        if (callback(item)){
-            mass.push(item)
-        }
-    } return mass
-}
-
-console.log(filter([11, 22, 33, 44, 55, 66, 77, 88], (item) => {
-    return item%2 === 0
-}));
-
-let userAge =filter(users, (user) => {
-    return user.age >= 30
-});
-console.log(userAge)
-
-
-
-// ще приклад
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+//
+//
+// function filter (arr, callback) {
+//     let mass = []
+//     for (const item of arr) {
+//         if (callback(item)){
+//             mass.push(item)
+//         }
+//     } return mass
+// }
+//
+// console.log(filter([11, 22, 33, 44, 55, 66, 77, 88], (item) => {
+//     return item%2 === 0
+// }));
+//
+// let userAge =filter(users, (user) => {
+//     return user.age >= 30
+// });
+// console.log(userAge)
+//
+//
+//
+// // ще приклад
 
 
