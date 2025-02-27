a4b6b7164b7deaa523c2789e666a051141050508
aleks1303 <aleksbulda13@gmail.com>
2025-02-27 14:24:28 +0200

    lesson 10 Events

--- lesson10/hw10/main.js
+++ lesson10/hw10/main.js
@@ -2,25 +2,47 @@
 // - Створити довільний елемент з id = text та створити кнопку.Використовуючи JavaScript, зробіть так, щоб при натисканні на кнопку зникав елемент з id="text".
 
 const text = document.getElementById('text')
-const button = document.createElement('button')
-button.innerText = 'Натисни і зникне'
+const button = document.getElementById('btn')
 button.addEventListener('click', function () {
     text.style.display = 'none'
 })
-document.body.appendChild(button)
-
-
 
 //         #j693ca8
 //     - створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
 
+const input = document.getElementById('i1')
+const button1 = document.getElementById('btn1')
+
+button1.onclick = function () {
+    if(!input.value) {
+        alert("you didn't enter age")
+    }
+    else if (input.value < 0) {
+        alert('age wrong')
+    } else if (input.value <= 18){
+        alert("you don't have 18 age! Sorry")
+    }else if (input.value >18 && input.value <= 120) {
+        alert('Ok. Your age is more then 18 age')
+    } else {alert('Maybe you already are very old or died')}
+    console.log(input.value)
+}
 
 
 // #ymAmN2xJ
-// Стоврити форму з трьома полями для name,sruname,age та кнопкою. При натисканні на кнопку зчитати данні з полів, та вивести об'єкт в документ. Іншими словами : заповниои форму, натиснули кнопку, під формою з'явився блок з вашим об'єктом
+// Створити форму з трьома полями для name, surname, age та кнопкою. При натисканні на кнопку зчитати дані з полів, та вивести об'єкт в документ. Іншими словами : заповнили форму, натиснули кнопку, під формою з'явився блок з вашим об'єктом
+// const form = document.getElementById('f2')
 //
+// const infoUser = document.getElementById('info-user')
+// form.onclick = function (ev) {
+//     ev.preventDefault()
+//     let user = {name: this.value, surname: this.value, age: this.age}
+//    localStorage.setItem("user", JSON.stringify(user))
+// }
+
+
+
 // #2VaLt4vDczH
-// є сторінка, на якій є блок, я кому знаходиьтся цифра. написати код, який при кожному перезавантажені сторінки буде додавати до неї +1
+// є сторінка, на якій є блок, я кому знаходиться цифра. написати код, який при кожному перезавантажені сторінки буде додавати до неї +1
 //
 // #LhSfdhM3
 // Є сторінка index.html (назва довільна), при відвідуванні якої в локальне сховще, в масив sessionsList зберігається інформація про дату та час відвідування сторінки. Є  сторінка sessionsListPage.html (назва довільна), при відвідуванні якої потрібно відмалювати всю інформацію про відвідування сторінки index.html. Інфу НЕ виводити в консоль, а малювати в DOM
