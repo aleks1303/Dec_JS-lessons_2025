90bb3a614c1b62043aa8e45de7f2801aed164ddb
aleks1303 <aleksbulda13@gmail.com>
2025-03-04 16:52:14 +0200

    lesson 11 fetch

---
+++ lesson11/lesson/main.js
@@ -0,0 +1,9 @@
+
+// ------------------------------------
+
+// fetch
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
