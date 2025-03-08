de6f925cc69478d0d022cb2236aacd4a4418b35a
aleks1303 <aleksbulda13@gmail.com>
2025-03-08 22:48:20 +0200

    lesson 11 fetch

--- lesson11/lesson/main.js
+++ lesson11/lesson/main.js
@@ -2,8 +2,63 @@
 // ------------------------------------
 
 // fetch
+//
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then((response) => response.json())
+//     .then((users) => {
+//         for (const {name} of users) {
+//             document.write(`<div>${name}</div>`)
+//         }
+//
+//     });
+//
+// fetch('https://dummyjson.com/products?limit=3&skip=3')
+//     .then(res => res.json())
+//     .then (({products}) => {
+//         for (const {id, brand, category, price, thumbnail} of products) {
+//             document.write(`<div>${id}___${brand} ${category} ${price}</div>`)
+//             document.write(`<img src="${thumbnail}" alt="#">`)
+//         }
+//
+//     });
+//
+// // Додати до сервера інформацію
+// fetch('https://dummyjson.com/products/add', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({
+//         title: 'BMW Pencil',
+//         /* other product data */
+//     })
+// })
+//     .then(res => res.json())
+//     .then(console.log);
+//
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
 
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+//--------------------------------------
+
+// URL
+//
+//searchParams
+//
+// // пошук за параметрами
+// let url = new URL('https://jsonplaceholder.typicode.com/users')
+// // додаємо до url параметри (key, value)
+// // і знаходимо одного user з id 4
+// url.searchParams.set("id", '4')
+//
+// // отримаємо 4
+// console.log(url.searchParams.get('id'));
+//
+//
+// fetch(url)
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+
+//--------------------------------------
+
+// regexp and post
 
